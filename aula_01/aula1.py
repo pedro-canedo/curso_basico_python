0e0c2ac20b4e5696a04fd6079861086ccab56d2c
pedro obalhe <pedro@MacBook-Pro-de-pedro.local>
2023-08-01 22:52:47 -0300

    feat: subindo aula 1

---
+++ aula_01/aula1.py
@@ -0,0 +1,36 @@
+
+# Exemplo 1: Hello World!
+print("Hello World!")
+
+# # Exemplo 2: Variáveis e input()
+# nome = input("Digite seu nome: ")
+
+# print("Olá, " + nome + "! (concatenação)")
+
+# print("Olá, {}! (format)".format(nome))
+
+# print(f"Olá, {nome}! (f-string)")
+
+# # EExemplos práticos de código
+# # Exemplo 1: Calculadora de IMC
+
+# def calcula_imc(peso, altura):
+#     return peso / altura ** 2
+
+# peso = float(input("Digite seu peso: "))
+# altura = float(input("Digite sua altura: "))
+# imc = calcula_imc(peso, altura)
+
+# print(f"Seu IMC é {imc:.2f}")
+# #o :.2f é para formatar o número com duas casas decimais
+
+# Exercício: Criar um programa que recebe o nome do usuário e o saúda com uma mensagem personalizada.
+
+# def saudacao(nome):
+#     print(f"Olá, {nome}!")
+    
+# nome = input("Digite seu nome: ")
+# saudacao(nome)
+
+
+
